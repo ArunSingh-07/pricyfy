ab806a42c81fa01d63e1370116a217d6543c5928
Arun Singh <arun987770@gmail.com>
2023-10-06 08:38:14 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        reactStrictMode: false,
+        serverActions: true,
+        serverComponentsExternalPackages: ['mongoose']
+      },
+      images: {
+        domains: ['m.media-amazon.com']
+      },
+}
 
 module.exports = nextConfig
